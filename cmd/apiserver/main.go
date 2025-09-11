90e2d6b48649c2c498e910f9599975f2c899e5de
Felix Matouschek <fmatouschek@redhat.com>
2025-09-11 16:32:20 +0200

    feat(apiserver): Implement custom Kubernetes API server for subresources
    Adds a complete custom API server implementation based on k8s.io/apiserver
    to serve VirtualMachineTemplate subresources. It includes storage backends,
    REST handlers, and API group registration.
    
    Signed-off-by: Felix Matouschek <fmatouschek@redhat.com>

---
+++ cmd/apiserver/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"os"
+
+	"github.com/spf13/pflag"
+	"k8s.io/klog/v2"
+
+	"kubevirt.io/client-go/kubecli"
+
+	templateapi "kubevirt.io/virt-template/api"
+	"kubevirt.io/virt-template/api/subresourcesv1alpha1"
+	"kubevirt.io/virt-template/client-go/template"
+
+	"kubevirt.io/virt-template/internal/apiserver"
+	"kubevirt.io/virt-template/internal/apiserver/openapi"
+	"kubevirt.io/virt-template/internal/apiserver/storage/virtualmachinetemplate"
+	templatescheme "kubevirt.io/virt-template/internal/scheme"
+)
+
+func main() {
+	s := apiserver.New()
+
+	s.AddFlags(pflag.CommandLine)
+	pflag.CommandLine.AddGoFlagSet(kubecli.FlagSet())
+	pflag.Parse()
+
+	virtClient, err := kubecli.GetKubevirtClient()
+	if err != nil {
+		klog.Fatalf("Failed to get virtClient: %v", err)
+	}
+	client, err := template.NewForConfig(virtClient.Config())
+	if err != nil {
+		klog.Fatalf("Failed to create client: %v", err)
+	}
+
+	scheme := templatescheme.New()
+	apiGroups := apiserver.APIGroups{
+		subresourcesv1alpha1.GroupVersion: {
+			templateapi.PluralResourceName:              virtualmachinetemplate.NewDummyREST(),
+			templateapi.PluralResourceName + "/process": virtualmachinetemplate.NewProcessREST(client),
+		},
+	}
+
+	if err := s.Run(
+		"virt-template-apiserver",
+		scheme, openapi.NewConfig(scheme), openapi.NewV3Config(scheme), apiGroups,
+	); err != nil {
+		os.Exit(1)
+	}
+}
